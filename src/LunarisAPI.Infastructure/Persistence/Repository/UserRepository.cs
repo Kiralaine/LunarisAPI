7921cf13b8e4a6327a0c9d8eb744ff8a5a3a0ef7
Kiralaine <k9034449@gmail.com>
2025-08-06 02:29:34 +0500

    [20%][WIP]

---
+++ src/LunarisAPI.Infastructure/Persistence/Repository/UserRepository.cs
@@ -0,0 +1,48 @@
+﻿using LunarisAPI.App.Interfaces;
+using LunarisAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LunarisAPI.Infastructure.Persistence.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        Task IUserRepository.BanAsync(long UserID)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IUserRepository.Delete(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IUserRepository.InsertAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<int> IUserRepository.SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        IQueryable<User> IUserRepository.SelectAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<User?> IUserRepository.SelectByIdAsync(long UserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IUserRepository.Update(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
