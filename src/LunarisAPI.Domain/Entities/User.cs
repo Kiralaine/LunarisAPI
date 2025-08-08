16bc0b1281a489e0e91c67e1c7b2270f75c996f2
Kiralaine <k9034449@gmail.com>
2025-08-08 16:02:42 +0500

    [WIP]

--- src/LunarisAPI.Domain/Entities/User.cs
+++ src/LunarisAPI.Domain/Entities/User.cs
@@ -9,11 +9,11 @@ namespace LunarisAPI.Domain.Entities
         public string?  ProfilePictureId { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
+        public string Password { get; set; }
+        public string Salt { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
         public string? Bio { get; set; }
-        public string Password { get; set; }
-        public string Salt { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public bool isBanned { get; set; } = false;
         public UserRole Role { get; set; }
